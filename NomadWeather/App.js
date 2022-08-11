dbbe0000badbdef8226d00aa178c3b0a90580314
isdiscodead <isdiscodead@korea.ac.kr>
2022-08-11 12:34:56 +0900

    -

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -35,7 +35,7 @@ export default function App() {
     setCity(location[0].city);
 
     // 날씨 data 가져오기
-    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${API_KEY}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
     const json = await response.json();
     setDays(json.daily);
   };
